8de0587dcd0d804abc05c3d9eef293c51ea2029a
crezin <cremilsonyahoo78@gmail.com>
2024-07-19 19:54:22 -0300

    segundo commit: conserta o arquivo Contapoupança.cpp

--- ContaPoupanca.cpp
+++ ContaPoupanca.cpp
@@ -1,18 +1,9 @@
-#ifndef CONTAPOUPANCA_H
-#define CONTAPOUPANCA_H
 
-#include "Conta.h"
 #include "ContaPoupanca.h"
 
+ContaPoupanca::ContaPoupanca(int numeroConta) : Conta(numeroConta) {}
 
-class ContaPoupanca : public Conta {
-public:
-    ContaPoupanca(int numeroConta);
-    void aplicarJuros(double taxa);
-};
-
-#endif // CONTAPOUPANCA_H
-
-ContaPoupanca::ContaPoupanca(int numeroConta) : Conta(numeroConta) {
-
+void ContaPoupanca::aplicarJuros(double taxa) {
+    saldo += saldo * taxa;
+    adicionarTransacao(Transacao("Juros", saldo * taxa));
 }
